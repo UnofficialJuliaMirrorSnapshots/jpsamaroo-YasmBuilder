554665e6db799dcc2f69de5a5be58bcb91c0f864
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-09 10:19:00 +0000

    Snapshot of branch master taken on 2019-05-09T06:18:59.319-04:00 from "https://github.com/UnofficialJuliaMirror/jpsamaroo-YasmBuilder"

---
+++ build_tarballs.jl
@@ -0,0 +1,48 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "YasmBuilder"
+version = v"1.3.0"
+
+# Collection of sources required to build YasmBuilder
+sources = [
+    "http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz" =>
+    "3dce6601b495f5b3d45b59f7d2492a340ee7e84b5beca17e48f862502bd5603f",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd yasm-1.3.0/
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
+make -j${nproc}
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:x86_64, :glibc),
+    Linux(:x86_64, :musl)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libyasm", :libyasm),
+    ExecutableProduct(prefix, "", :yasm),
+    ExecutableProduct(prefix, "", :ytasm),
+    LibraryProduct(prefix, "libyasmstd", :libyasmstd),
+    ExecutableProduct(prefix, "", :vsyasm)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
